b52b034071d46809c92dc38b930f234fe64586ce
fatehi-ali <79412432+fatehi-ali@users.noreply.github.com>
2021-04-24 06:52:51 -0700

    Creat function get_header, get_footer, get_sidebar, get_template_part
    Creat function get_header, get_footer, get_sidebar, get_template_part

---
+++ includes/view/templates/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="utf-8">
+    <title>Academy-Bime</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="assets/img/1.png" type="image/png"/>
+    <link rel="stylesheet" href="assets/css/respansive.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+
+</head>
+<body>
+<header id="top">
+    <img src="img/2.png" alt="">
+    <h1>آکـادمی بیــــمه</h1>
+    <h2>آموزش فــــــروش و بازایــابی بیمه</h2>
+</header>
+<nav>
+    <div id="mobail">
+        منـــــو
+        <ul>
+            <li class=""><a href="index.html">صفحه اصلی</a></li>
+
+        </ul>
+    </div>
+    <ul id="mainmenu">
+        <li class=""><a href="index.html">صفحه اصلی</a></li>
+    </ul>
+</nav>
