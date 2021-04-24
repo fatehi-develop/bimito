d25cb2683f5dbe36f00f6a2daa84edada781dc36
fatehi-ali <79412432+fatehi-ali@users.noreply.github.com>
2021-04-24 06:48:10 -0700

    Creat function get_header, get_footer, get_sidebar, get_template_part
    Creat function get_header, get_footer, get_sidebar, get_template_part

--- includes/function.php
+++ includes/function.php
@@ -1,5 +1,33 @@
 <?php
 include 'DB.php';
+if( ! function_exists('get_header') ){
+    function get_header($name = null, $args = array()){
+        if( isset( $name ) )
+            $name = "-{$name}";
+        include "view/templates/header{$name}.php";
+    }
+}
+if( ! function_exists('get_sidebar') ){
+    function get_sidebar($name = null, $args = array()){
+        if( isset( $name ) )
+            $name = "-{$name}";
+        include "view/templates/sidebar{$name}.php";
+    }
+}
+if( ! function_exists('get_footer') ){
+    function get_footer($name = null, $args = array()){
+        if( isset( $name ) )
+            $name = "-{$name}";
+        include "view/templates/footer{$name}.php";
+    }
+}
+if( ! function_exists('get_template_part') ){
+    function get_template_part($slug, $name = null, $args = array()){
+        if( isset( $name ) )
+            $name = "-{$name}";
+        include "view/templates/{$slug}{$name}.php";
+    }
+}
 if (!function_exists('alertTemplate')) {
     function alertTemplate($text, $type = 'error')
     {
--- includes/view/listuser.php
+++ includes/view/listuser.php
@@ -1,64 +1,4 @@
-<!DOCTYPE html>
-<html lang="fa">
-<head>
-    <meta charset="utf-8">
-    <title>Academy-Bime</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="assets/img/1.png" type="image/png"/>
-    <link rel="stylesheet" href="assets/css/respansive.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
-</head>
-<body>
-<header id="top">
-    <img src="img/2.png" alt="">
-    <h1>آکـادمی بیــــمه</h1>
-    <h2>آموزش فــــــروش و بازایــابی بیمه</h2>
-</header>
-<nav>
-    <div id="mobail">
-        منـــــو
-        <ul>
-            <li class=""><a href="index.html">صفحه اصلی</a></li>
-            <li class=""><a href="articel.html">مقالات</a>
-                <ul>
-                    <li><a href="articel.html">بیمه های عمر</a></li>
-                    <li><a href="">بیمه های مسئولیت</a>
-                        <ul>
-                            <li><a href="">بیمه مسئولیت کارفرما</a></li>
-                            <li><a href="">بیمه مسئولیت مهندسین ناظر</a></li>
-                        </ul>
-                    </li>
-                    <li><a href=""> بیمه شخص ثالث </a></li>
-                </ul>
-            </li>
-            <li><a href="">آموزش فروش </a></li>
-            <li><a href="">پادکست ها </a></li>
-            <li><a href="AboutUs.html">درباره ما</a></li>
-            <li><a href="#">ارتباط با ما </a></li>
-        </ul>
-    </div>
-    <ul id="mainmenu">
-        <li class=""><a href="index.html">صفحه اصلی</a></li>
-        <li class=""><a href="articel.html">مقالات</a>
-            <ul>
-                <li><a href="articel.html">بیمه های عمر</a></li>
-                <li><a href="">بیمه های مسئولیت</a>
-                    <ul>
-                        <li><a href="">بیمه مسئولیت کارفرما</a></li>
-                        <li><a href="">بیمه مسئولیت مهندسین ناظر</a></li>
-                    </ul>
-                </li>
-                <li><a href=""> بیمه شخص ثالث </a></li>
-            </ul>
-        </li>
-        <li><a href="">آموزش فروش </a></li>
-        <li><a href="">پادکست ها </a></li>
-        <li><a href="AboutUs.html">درباره ما</a></li>
-        <li><a href="ContactUs.html">ارتباط با ما </a></li>
-    </ul>
-</nav>
+<?php get_header();?>
 <h1 class="callus">لیست کاربران</h1>
 <main class="mainarticel">
     <div>
@@ -115,14 +55,4 @@
         </div>
     </div>
 </main>
-<span class="footer"> کلیه حقوق برای <strong> بیمه</strong> محفوظ می باشد </span>
-<!--      <ul class="social">-->
-<!--        <li> <img class="telegram" src="img/telegram.png" alt=""></li>-->
-<!--        <li><img class="youtube" src="img/youtube.png" alt=""></li>-->
-<!--        <li><img class="whatsapp" src="img/whatsapp.png" alt=""></li>-->
-<!--    </ul>-->
-<p id="gotop">
-    <a href="#top"><img class="top" src="img/5.png" alt=""></a>
-</p>
-</body>
-</html>
+<?php get_footer();?>
